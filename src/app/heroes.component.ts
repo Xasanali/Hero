b48066243305d80a7e2f897fe65ee89719df3741
Хасан Шарапов <kh.sharapov@bepro.uz>
2017-06-16 17:56:57 +0500

    16.06.2017 works

--- src/app/heroes.component.ts
+++ src/app/heroes.component.ts
@@ -18,27 +18,27 @@ export class HeroesComponent implements OnInit {
   selectedHero: Hero;
 
   constructor(
-    private router: Router, 
-    private heroService: HeroService    ) { }
+    private router: Router,
+    private heroService: HeroService) { }
 
-    add(name: string): void {
-      name=name.trim();
-      if(!name) { return; }
-      this.heroService.create(name)
-        .then(hero => { 
-          this.heroes.push(hero);
-          this.selectedHero=null;
-        });
-    }
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.create(name)
+      .then(hero => {
+        this.heroes.push(hero);
+        this.selectedHero = null;
+      });
+  }
 
-    delete(hero: Hero): void { 
-      this.heroService
-          .delete(hero.id)
-          .then(()=> {
-            this.heroes = this.heroes.filter(h=> h!==hero);
-            if (this.selectedHero ===hero) { this.selectedHero = null;}
-          });
-    }
+  delete(hero: Hero): void {
+    this.heroService
+      .delete(hero.id)
+      .then(() => {
+        this.heroes = this.heroes.filter(h => h !== hero);
+        if (this.selectedHero === hero) { this.selectedHero = null; }
+      });
+  }
 
   getHeroes(): void {
     this.heroService.getHeroes().then(heroes => this.heroes = heroes);
@@ -52,7 +52,7 @@ export class HeroesComponent implements OnInit {
   gotoDetail(): void {
     this.router.navigate(['/detail', this.selectedHero.id]);
   }
-} 
+}
 
 
 
